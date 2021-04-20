cf57ad0c538a98293e0ed8ece4a797369a6bf87f
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2021-04-20 13:23:47 +0200

    Introduce 16-bit SPI functions

--- tb/firmware/include/spi.h
+++ tb/firmware/include/spi.h
@@ -7,8 +7,14 @@ void spi_deinit(void);
 uint8_t spi_read_byte(void);
 void spi_write_byte(uint8_t out);
 
-void spi_read(uint8_t *buf, uint32_t length);
-void spi_write(const uint8_t *buf, uint32_t length);
+void spi_read_bytes(uint8_t *buf, uint32_t length);
+void spi_write_bytes(const uint8_t *buf, uint32_t length);
 
+uint16_t spi_read_word(void);
+void spi_write_word(uint16_t out);
+
+void spi_read_words(uint16_t *buf, uint32_t length);
+void spi_write_words(const uint16_t *buf, uint32_t length);
+void spi_transfer_words(const uint16_t *in, uint16_t *out, unsigned n);
 
 #endif
--- tb/firmware/src/spi.c
+++ tb/firmware/src/spi.c
@@ -10,6 +10,8 @@
 
 #define SPI_DUMMY 0x55
 
+#define SPI_FRAME_SIZE 16
+
 #ifndef UNIT_TESTING
 
 #define SPI_CR_BR_DIV_2   (0x0 << SPI_CR1_BR_Pos)
@@ -24,7 +26,9 @@
 void spi_init(void)
 {
     RCC->APBENR2 |= RCC_APBENR2_SPI1EN;
+#if (SPI_FRAME_SIZE == 8)
     SPI1->CR2 |= SPI_CR2_FRXTH;
+#endif
     SPI1->CR1 = SPI_CR_BR_DIV_8 | SPI_CR1_MSTR | SPI_CR1_SSM | SPI_CR1_SSI | SPI_CR1_SPE;
 }
 
@@ -46,29 +50,70 @@ static void spi_wait_for_busy(void)
     }
 }
 
-static uint8_t spi_receive(void)
+static uint8_t spi_receive_byte(void)
 {
     return *((volatile uint8_t*)&SPI1->DR);
 }
 
-static void spi_send(uint8_t data)
+static void spi_send_byte(uint8_t data)
 {
     *((volatile uint8_t*)&SPI1->DR) = data;
 }
 
-void spi_transfer(int8_t *in, uint8_t *out, unsigned n)
+static uint16_t spi_receive_word(void)
+{
+    return SPI1->DR;
+}
+
+static void spi_send_word(uint16_t data)
+{
+    SPI1->DR = data;
+}
+
+void spi_transfer_bytes(const uint8_t *out, uint8_t *in, unsigned n)
 {
     unsigned ind_in = 0;
     unsigned ind_out = 0;
 
+#if (SPI_FRAME_SIZE == 16)
+    SPI1->CR2 |= SPI_CR2_FRXTH;
+#endif
+
     while(ind_in < n) {
         if((ind_out < n) && (SPI1->SR & SPI_SR_TXE)) {
-            spi_send(in[ind_in++]);
+            spi_send_byte(out[ind_out++]);
         }
         if((ind_in < n) && (SPI1->SR & SPI_SR_RXNE)) {
-            out[ind_out++] = spi_receive();
+            in[ind_in++] = spi_receive_byte();
         }
     }
+
+#if (SPI_FRAME_SIZE == 16)
+     SPI1->CR2 &= ~SPI_CR2_FRXTH;
+#endif
+}
+
+void spi_transfer_words(const uint16_t *out, uint16_t *in, unsigned n)
+{
+    unsigned ind_in = 0;
+    unsigned ind_out = 0;
+
+#if (SPI_FRAME_SIZE == 8)
+    SPI1->CR2 &= ~SPI_CR2_FRXTH;
+#endif
+
+    while(ind_in < n) {
+        if((ind_out < n) && (SPI1->SR & SPI_SR_TXE)) {
+            spi_send_word(out[ind_out++]);
+        }
+        if((ind_in < n) && (SPI1->SR & SPI_SR_RXNE)) {
+            in[ind_in++] = spi_receive_word();
+        }
+    }
+
+#if (SPI_FRAME_SIZE == 8)
+    SPI1->CR2 |= SPI_CR2_FRXTH;
+#endif
 }
 
 void spi_deinit(void)
@@ -77,7 +122,7 @@ void spi_deinit(void)
     spi_wait_for_txe();
     spi_wait_for_busy();
 
-    uint32_t dummy __attribute__((unused)) = spi_receive();
+    uint32_t dummy __attribute__((unused)) = spi_receive_byte();
 
     SPI1->CR1 = 0x00;
     RCC->APBENR2 &= ~RCC_APBENR2_SPI1EN;
@@ -88,48 +133,167 @@ void spi_deinit(void)
 void spi_wait_for_txe(void);
 void spi_wait_for_rxne(void);
 void spi_wait_for_busy(void);
-uint8_t spi_receive(void);
-void spi_send(uint8_t data);
+uint8_t spi_receive_byte(void);
+void spi_send_byte(uint8_t data);
+uint8_t spi_receive_word(void);
+void spi_send_word(uint8_t data);
 
 #endif
 
 void spi_write_byte(uint8_t out) {
+#if (SPI_FRAME_SIZE == 16)
+    SPI1->CR2 |= SPI_CR2_FRXTH;
+#endif
+
     spi_wait_for_txe();
-    spi_send(out);
+    spi_send_byte(out);
 
     spi_wait_for_rxne();
-    uint32_t dummy __attribute__((unused)) = spi_receive();
+    uint32_t dummy __attribute__((unused)) = spi_receive_byte();
+
+#if (SPI_FRAME_SIZE == 16)
+    SPI1->CR2 &= ~SPI_CR2_FRXTH;
+#endif
+
 }
 
 uint8_t spi_read_byte(void)
 {
+#if (SPI_FRAME_SIZE == 16)
+    SPI1->CR2 |= SPI_CR2_FRXTH;
+#endif
+
     spi_wait_for_txe();
 
-    spi_send(SPI_DUMMY);
+    spi_send_byte(SPI_DUMMY);
 
     spi_wait_for_rxne();
 
-    return spi_receive();
+#if (SPI_FRAME_SIZE == 16)
+    SPI1->CR2 &= ~SPI_CR2_FRXTH;
+#endif
+
+    return spi_receive_byte();
 }
 
-void spi_read(uint8_t *buf, uint32_t length)
+void spi_read_bytes(uint8_t *buf, uint32_t length)
 {
+#if (SPI_FRAME_SIZE == 16)
+    SPI1->CR2 |= SPI_CR2_FRXTH;
+#endif
+
     for(uint32_t i = 0; i < length; i++) {
         spi_wait_for_txe();
-        spi_send(SPI_DUMMY);
+        spi_send_byte(SPI_DUMMY);
 
         spi_wait_for_rxne();
-        buf[i] = spi_receive();
+        buf[i] = spi_receive_byte();
     }
+
+#if (SPI_FRAME_SIZE == 16)
+    SPI1->CR2 &= ~SPI_CR2_FRXTH;
+#endif
 }
 
-void spi_write(const uint8_t *buf, uint32_t length)
+void spi_write_bytes(const uint8_t *buf, uint32_t length)
 {
+#if (SPI_FRAME_SIZE == 16)
+    SPI1->CR2 |= SPI_CR2_FRXTH;
+#endif
+
     for(uint32_t i = 0; i < length; i++) {
         spi_wait_for_txe();
-        spi_send(buf[i]);
+        spi_send_byte(buf[i]);
 
         spi_wait_for_rxne();
-        uint32_t dummy __attribute__((unused)) = spi_receive();
+        uint32_t dummy __attribute__((unused)) = spi_receive_byte();
     }
+
+#if (SPI_FRAME_SIZE == 16)
+    SPI1->CR2 &= ~SPI_CR2_FRXTH;
+#endif
+}
+
+
+
+void spi_write_word(uint16_t out) {
+#if (SPI_FRAME_SIZE == 8)
+    SPI1->CR2 &= ~SPI_CR2_FRXTH;
+#endif
+
+    spi_wait_for_txe();
+    spi_send_word(out);
+
+    spi_wait_for_rxne();
+    uint32_t dummy __attribute__((unused)) = spi_receive_word();
+
+#if (SPI_FRAME_SIZE == 8)
+    SPI1->CR2 |= SPI_CR2_FRXTH;
+#endif
+}
+
+uint16_t spi_read_word(void)
+{
+#if (SPI_FRAME_SIZE == 8)
+    SPI1->CR2 &= ~SPI_CR2_FRXTH;
+#endif
+
+    spi_wait_for_txe();
+
+    spi_send_word(SPI_DUMMY);
+
+    spi_wait_for_rxne();
+
+#if (SPI_FRAME_SIZE == 8)
+    SPI1->CR2 |= SPI_CR2_FRXTH;
+#endif
+
+    return spi_receive_word();
+}
+
+void spi_read_words(uint16_t *buf, uint32_t length)
+{
+#if (SPI_FRAME_SIZE == 8)
+    SPI1->CR2 &= ~SPI_CR2_FRXTH;
+#endif
+
+    for(uint32_t i = 0; i < length; i++) {
+        spi_wait_for_txe();
+        spi_send_word(SPI_DUMMY);
+
+        spi_wait_for_rxne();
+        buf[i] = spi_receive_word();
+    }
+
+#if (SPI_FRAME_SIZE == 8)
+    SPI1->CR2 |= SPI_CR2_FRXTH;
+#endif
+}
+
+void spi_write_words(const uint16_t *buf, uint32_t length)
+{
+#if (SPI_FRAME_SIZE == 8)
+    SPI1->CR2 &= ~SPI_CR2_FRXTH;
+#endif
+
+    unsigned ind_in = 0;
+    unsigned ind_out = 0;
+
+#if (SPI_FRAME_SIZE == 8)
+    SPI1->CR2 &= ~SPI_CR2_FRXTH;
+#endif
+
+    while(ind_in < length) {
+        if((ind_out < length) && (SPI1->SR & SPI_SR_TXE)) {
+            spi_send_word(buf[ind_out++]);
+        }
+        if((ind_in < length) && (SPI1->SR & SPI_SR_RXNE)) {
+            uint16_t dummy __attribute__((unused)) = spi_receive_word();
+            ind_in++;
+        }
+    }
+
+#if (SPI_FRAME_SIZE == 8)
+    SPI1->CR2 |= SPI_CR2_FRXTH;
+#endif
 }
