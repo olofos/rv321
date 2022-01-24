0261e7460809de02e90e3750957c649613a2cf03
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2022-01-24 17:14:00 +0100

    Removed counter example component

--- microcode-app/client/src/app/store.js
+++ microcode-app/client/src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import timingDiagramReducer from '../features/timing-diagram/timingDiagramSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     timingDiagram: timingDiagramReducer,
   },
 });
